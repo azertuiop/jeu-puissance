8dd8e78304087916d20a80dcc32413883745d204
mustafa.alrubi@gmail.com <mustafa.alrubi@gmail.com@9d835eca-7466-56e5-ae84-14ce718f9639>
2011-11-28 14:39:08 +0000

    correction probleme chargement des images dans sdljeu.c

--- trunk/Init_prog_puissance/src/sdlJeu.c
+++ trunk/Init_prog_puissance/src/sdlJeu.c
@@ -27,15 +27,15 @@ void sdljeuInit(sdlJeu *pSdlJeu)
 	assert( pSdlJeu->surface_ecran!=NULL);
 	SDL_WM_SetCaption( "Puissance 4", NULL );
 
-	pSdlJeu->surface_puissance = SDL_load_image("/data/images/acceuil1.bmp");
+	pSdlJeu->surface_puissance = SDL_load_image("data/images/acceuil1.bmp");
 	if (pSdlJeu->surface_puissance==NULL)
-		pSdlJeu->surface_puissance = SDL_load_image("/data/images/acceuil1.bmp");
+		pSdlJeu->surface_puissance = SDL_load_image("data/images/acceuil1.bmp");
     assert( pSdlJeu->surface_puissance!=NULL);
 
-	pSdlJeu->surface_case = SDL_load_image("/data/images/casevide.bmp");
+	pSdlJeu->surface_case = SDL_load_image("data/images/casevide.bmp");
 	if (pSdlJeu->surface_case==NULL)
-		pSdlJeu->surface_case = SDL_load_image("/data/images/casevide.bmp");
-	//assert( pSdlJeu->surface_case!=NULL);
+		pSdlJeu->surface_case = SDL_load_image("data/images/casevide.bmp");
+	assert( pSdlJeu->surface_case!=NULL);
 }
 
 
