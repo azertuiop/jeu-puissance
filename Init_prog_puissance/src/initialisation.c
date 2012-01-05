b7aba63e2bcd550fa4ae91a3a5918024b4e38e0b
omid.norouzzadeh <omid.norouzzadeh@9d835eca-7466-56e5-ae84-14ce718f9639>
2012-01-05 18:36:35 +0000

    suprimer les fichier .c

--- Init_prog_puissance/src/initialisation.c
+++
@@ -1,8 +0,0 @@
-#ifndef _INIT_H
-#define _INIT_H
-
-#include <stdio.h>
-
-void init_score(Couleur couleur, SDL-Surface *ecran, SDL_Surface *imageFond, SDL_Rect positionFond, unsigned char tourr);
-/* j'ai ajouté ceci*/
-#endif
